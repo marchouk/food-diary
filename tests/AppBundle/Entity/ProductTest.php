4ff38ce5503b62b3c43093b3569d9f60380f4bb2
marchouk <mehdimarchouk@gmail.com>
2018-01-19 22:15:35 +0000

    Add product class and its Test class

---
+++ tests/AppBundle/Entity/ProductTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\AppBundle\Entity;
+
+
+use AppBundle\Entity\Product;
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function testComputeTVAFoodProduct()
+    {
+        $product = new Product('Un produit', Product::FOOD_PRODUCT, 20);
+
+        $this->assertSame(1.1, $product->computeTVA());
+    }
+
+    public function testComputeTVAOtherProduct()
+    {
+        $product = new Product('Un autre produit', 'Un autre type de produit', 20);
+
+        $this->assertSame(3.92, $product->computeTVA());
+    }
+
+    public function testNegativePriceComputeTVA()
+    {
+        $product = new Product('Un produit', Product::FOOD_PRODUCT, -20);
+
+        $this->expectException('LogicException');
+
+        $product->computeTVA();
+    }
+
+}
